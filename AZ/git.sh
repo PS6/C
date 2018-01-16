2433b693d2e96f5efc2eb7f8153e5b69ac7c1813
PS6 <p@shah.pm>
2018-01-16 11:54:52 +0100

    Update git.sh
    added runtime option

--- AZ/git.sh
+++ AZ/git.sh
@@ -9,6 +9,8 @@ rgname=ps6c_RG
 webappname=ps6c
 # FQDN requires SKU >= SHARED and cname in domain
 fqdn=p.s6.com
+# run az webapp list-runtimes to get a list of available runtimes
+runtimestr="php|7.1"
 
 # Create a resource group.
 az group create --location westeurope --name $rgname
@@ -16,7 +18,8 @@ az group create --location westeurope --name $rgname
 az appservice plan create --name $webappname --resource-group $rgname --sku FREE
 
 # Create a web app.
-az webapp create --name $webappname --resource-group $rgname --plan $webappname
+az webapp create --name $webappname --resource-group $rgname --plan $webappname \
+--runtime $runtimestr
 
 # Configure continuous deployment from GitHub.
 # --git-token parameter is required only once per Azure account (Azure remembers token).
