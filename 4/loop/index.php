f0fc6b39cfebbbe17628b18eb5bbf5a89b96dbf4
PS6 <p@shah.pm>
2018-08-16 11:18:52 +0200

    Create index.php

---
+++ 4/loop/index.php
@@ -0,0 +1,31 @@
+<?php
+date_default_timezone_set("UTC");
+$qstr = $_SERVER["QUERY_STRING"];
+parse_str($qstr, $output);
+$Token = $output['token'];
+$sigToken = hash_hmac('sha256', $Token, "B7&1(y^%mm0a12g&!09-g6yh4d");
+
+$snow = time();
+$cnow = (int)($output['time']/1000);
+$titlestr = "<title>". date("H:i:s e",$snow) ." - title </title>";
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<? echo $titlestr ?>
+</head>
+<body>
+<? echo "Token    : " . $Token ?> <br>
+<? echo "sigToken : " . $sigToken ?> <br>
+<script>
+var qstr = window.location.hash.substring(1);
+var redstr = "../#" + qstr;
+//setTimeout(function(){alert(redstr)}, 10000);
+document.writeln('<br><a href=' + redstr + '>direct redirect</a>');
+setTimeout(function(){location.assign(redstr)}, 590000);
+</script>
+</div>
+</body>
+</html>
